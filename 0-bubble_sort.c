351fe8d4ac4082b2ec3361d0c5c39a76745d33ff
AxiomDays <tobeoke@gmail.com>
2023-12-17 02:16:33 +0100

    bubble sort completed?

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,7 +3,11 @@
 #include "sort.h"
 
 /**
- * main - Entry point
+ * bubble_sort - Entry point
+ *
+ * Description: 'function that sorts an array using the bubble sort algorithm'
+ * @array: int *array (array to be sorted)
+ * @size: size_t size (Size of array)
  *
  * Return: Always 0
  */
@@ -11,18 +15,20 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i;
 	size_t j;
-	long unsigned int temp;
+	unsigned long int temp;
 
 
-	for (j = 0; j < (size-1); j++){
-		for (i = 0; i < (size-1); i++){
+	for (j = 0; j < (size - 1); j++)
+	{
+		for (i = 0; i < (size - 1); i++)
+		{
 			if (array != NULL)
 			{
-				if (array[i] > array[i+1])
+				if (array[i] > array[i + 1])
 				{
 					temp = array[i];
-					array[i] = array[i+1];
-					array[i+1] = temp;
+					array[i] = array[i + 1];
+					array[i + 1] = temp;
 					print_array(array, size);
 				}
 			}
