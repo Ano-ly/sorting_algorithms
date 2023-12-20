25adb8c315f70b16b7eba5a177329243df02dda5
AxiomDays <tobeoke@gmail.com>
2023-12-20 23:35:43 +0100

    task100 implementing shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+void shell_sort(int *array, size_t size)
+{
+	long unsigned int i, j;
+	long unsigned int interval = 1;
+	long unsigned int intcount = 1;
+	long unsigned int intbuff = 1;
+	int temp;
+	
+
+	if (array == NULL){
+		return;
+	}
+
+	while (intbuff < size){
+	interval = intbuff;
+	intbuff = (power(3, intcount) - 1)/2;
+	intcount++;
+	}
+
+	while (interval > 0)
+	{
+		for (i=interval; i < (size); i++)
+		{
+			temp = array[i];
+			for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
+			{
+				array[j] = array[j-interval];
+			}
+			array[j] = temp;
+		}
+		interval = interval/3;
+		print_array(array, size);
+	}
+	
+	return;
+
+}
+
+int power(int value, int power)
+{
+	int j;
+	int temppower = value;
+	for (j = 0; j < power; j++)
+	{
+		value=value * temppower;
+	}
+	return value;
+}
--- sort.h
+++ sort.h
@@ -16,11 +16,13 @@ void insert_at_middle(listint_t **list, listint_t *Y, listint_t *Z);
 void selection_sort(int *array, size_t size);
 int mathmin(int *array, size_t size, unsigned long int counter);
 
-
 void quick_sort(int *array, size_t size);
 void sort_recursive(int *array, size_t start, size_t end, size_t size);
 size_t surround_pivot(int *array, size_t start, size_t end, size_t size);
 void swap_em(int *array, size_t swap1, size_t swap2, size_t size);
 int check_sorted(int *array, size_t size);
+
+void shell_sort(int *array, size_t size);
+int power(int value, int power);
 
 #endif
